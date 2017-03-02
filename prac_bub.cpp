310d41d0620aece37cb585f325cddf02a9694de2
shotaTJ <shotatuzi@gmail.com>
2017-03-02 12:57:05 +0900

    First commit

---
+++ prac_bub.cpp
@@ -0,0 +1,14 @@
+#define GLIBCXX_DEBUG
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define REP(i, n) for(int i=0; i < (int)n; ++i)
+#define FOR(i, c) for(__typeof( (c).being())i = (c).begin(); i != (c).end(); ++i )
+#define ALL(c) (c).begin(), (c).end()
+
+int main()
+{
+  
+}
