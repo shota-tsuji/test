2ae6e92b2f61f6456f163280282208d61460a3cd
shotaTJ <shotatuzi@gmail.com>
2017-03-09 16:48:57 +0900

    Adding a new directory ./mpi/ and a new file.

---
+++ mpi/intervals_mpi.cpp
@@ -0,0 +1,10 @@
+# include <cmath>
+# include <cstdlib>
+# include <ctime>
+# include <iomanip>
+# include <iostream>
+//# include <mpi.h>
+
+using namespace std;
+
+int main ( int argc, char *argv[] );
